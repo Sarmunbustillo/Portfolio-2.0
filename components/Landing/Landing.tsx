d97b20fa0e453b7f2d709abb1925dc302920357b
Sarmun Bustillo <sarmun.b@gmail.com>
2022-07-23 20:43:17 +0200

    Move to PNPM

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -5,7 +5,7 @@ import ContactButton from '../ContactButton';
 import ToggleTheme from '../ToggleTheme';
 
 export default function Landing() {
-    const sarRef = useRef(null);
+    const sarRef = useRef() as React.MutableRefObject<HTMLDivElement>;
     useParallax(sarRef);
     return (
         <section className={styles.landing}>
@@ -13,11 +13,16 @@ export default function Landing() {
                 <div ref={sarRef} className={styles.descripton}>
                     <h1>Sarmun Bustillo</h1>
                     <p>
-                        I'm a Self-taught Frontend Web-Dev based in Hamburg
+                        I'm a Frontend Web-Dev based in Hamburg
                         devoted to crafting exceptional web experiences.
                         Currently, I'm focused on building accessible, stylish
                         and meaningful web products.
                     </p>
+                    <span className="button-wrapper-border">
+                        <a href="mailto:sarmun.b@gmail.com" className="button ">
+                            Get in Touch
+                        </a>
+                    </span>
                 </div>
             </div>
             <div className={styles.image} />
