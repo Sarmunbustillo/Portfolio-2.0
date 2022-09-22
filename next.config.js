dff09cc729734e4b069372e21c37a7d23e49beeb
Sarmun Bustillo <sarmun.b@gmail.com>
2022-09-22 17:10:58 +0200

    Packages update

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
-module.exports = {
-    compiler: {
-        // ssr and displayName are configured by default
-        styledComponents: true,
-    },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
     images: {
         domains: ['www.datocms-assets.com'],
-    },
-    experimental: {
-        images: {
-            unoptimized: true,
-        },
     },
 
     reactStrictMode: true,
 };
+
+module.exports = nextConfig;
